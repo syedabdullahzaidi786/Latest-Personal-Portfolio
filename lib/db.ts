20bce9fb412fbe6d73900522f0fccbb4b47b1711
syedabdullahzaidi786 <syedabdullahzaidi786@gmail.com>
2026-07-23 15:06:17 +0500

    fix errors

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/prisma/client';
+import { PrismaClient } from '@prisma-client';
 import { Pool } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import bcrypt from 'bcryptjs';

d34c83916b5bb89e10024172eebb1071a2ffc063
syedabdullahzaidi786 <syedabdullahzaidi786@gmail.com>
2026-07-23 16:31:00 +0500

    roboto added valid url

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://syedabdullahzaidi.vercel.app";
+const siteUrl = "https://latest-personal-portfolio-gamma.vercel.app";
 
 export default function robots(): MetadataRoute.Robots {
   return {
